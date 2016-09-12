9b5e2d40139d16d039f3dbb8aedd8f3644314766
213-20\student <roshan_isaac@hotmail.com>
2016-09-12 10:36:52 -0400

    no message

---
+++ Level1/Level1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Level1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("YOU DID IT!");
+            Close();
+        }
+        private void MoveToStart()
+        {
+            Point StartingPoint = panel1.Location;
+            StartingPoint.Offset(10, 10);
+            Cursor.Position = PointToScreen(StartingPoint);
+        }
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            MoveToStart();
+
+        }
+        }
+}
